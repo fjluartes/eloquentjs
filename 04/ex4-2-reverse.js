b897230b5c50fcbef3fc936d7692f44016a571bc
fjgl <fjluartes@gmail.com>
2020-11-23 00:20:43 +0000

    Adds answers to exercises in chapter 4

--- 04/ex4-2-reverse.js
+++ 04/ex4-2-reverse.js
@@ -7,3 +7,15 @@ function reverseArray(arr) {
   return resArr;
 }
 console.log(reverseArray([1, 2, 3, 4, 5]));
+
+function reverseArrayInPlace(arr) {
+  let l = arr.length / 2;
+  let end = arr.length - 1;
+  for (let i = 0, j = end; i < l; i++, j--) {
+    let temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+  }
+  return arr;
+}
+console.log(reverseArrayInPlace([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
---
+++ 04/ex4-3-list.js
@@ -0,0 +1,1 @@
+// List: Create functions arrayToList and listToArray
